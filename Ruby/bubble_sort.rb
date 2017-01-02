be51dff6721ec0ef6f22f102a8fa464f90c499cb
Siansor <shapalov@gmail.com>
2017-01-02 23:18:07 +0200

    Add bubble sort

---
+++ Ruby/bubble_sort.rb
@@ -0,0 +1,20 @@
+def bubble_sort(array)
+  n = array.length
+  loop do
+    swapped = false
+
+    (n-1).times do |i|
+      if array[i] > array[i+1]
+        array[i], array[i+1] = array[i+1], array[i]
+        swapped = true
+      end
+    end
+
+    break if not swapped
+  end
+
+  p array
+end
+
+array = [7,3,22,4,12,46,88,323,4,1,64,7]
+bubble_sort(array)
