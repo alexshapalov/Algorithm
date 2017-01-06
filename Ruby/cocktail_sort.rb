f88bed916a499f1cd701c2cd7e78b4d6ec39f8c9
Siansor <shapalov@gmail.com>
2017-01-06 23:50:27 +0200

    Add cocktail_sort

---
+++ Ruby/cocktail_sort.rb
@@ -0,0 +1,20 @@
+def cocktail_sort!
+    f  = 0
+    while f < self.length/2
+      i = 0
+      while i < self.length - 1
+        self[i], self[i+1] = self[i+1], self[i] if self[i] > self[i+1]
+        i += 1;
+      end
+      t = self.length - 1
+      while t > 0
+         self[t], self[t-1] = self[t-1], self[t] if self[t] < self[t-1]
+         t -= 1
+      end
+      f += 1
+    end
+    self
+  end
+
+# http://en.wikipedia.org/wiki/Cocktail_sort
+# https://upload.wikimedia.org/wikipedia/commons/e/ef/Sorting_shaker_sort_anim.gif
--- Ruby/insert_sort.rb
+++ Ruby/insert_sort.rb
@@ -1,14 +1,19 @@
-def insert_sort!
-  (0...self.length).to_a.each do |j|
-    key = self[j]
-    i = j - 1;
-    while i >= 0 and self[i] > key
-      self[i+1] = self[i]
-      i = i-1
+class Array
+  def insert_sort!
+    (0...self.length).to_a.each do |j|
+      key = self[j]
+      i = j - 1;
+      while i >= 0 and self[i] > key
+        self[i+1] = self[i]
+        i = i-1
+      end
+      self[i+1] = key
     end
-    self[i+1] = key
+    self
   end
-  self
 end
 
 # https://ruby-china-files.b0.upaiyun.com/photo/2014/9c37b8be54c219e9edc5605376cadeeb.gif
+
+arr = [4,2,4,6,8,9,33,22,334,2,3,8,7,53,13]
+p arr.insert_sort!
